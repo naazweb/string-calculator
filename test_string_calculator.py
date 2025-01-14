e3f94ad68f1c8208401e12b03ef66526b1c1b4f8
naazweb <103880758+naazweb@users.noreply.github.com>
2025-01-14 16:17:51 +0530

    test for custom delimiter

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -20,6 +20,9 @@ class TestStringCalculator(unittest.TestCase):
     def test_newline_delimiter(self):
         self.assertEqual(add('1\n2,3'), 6)
 
+    def test_custom_delimiter(self):
+        self.assertEqual(add('//;\n1;2'), 3)
+
 
 # Run the tests
 if __name__ == '__main__':
